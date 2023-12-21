0fb483491fc3d1fb3830b5a8294c422e8dc1a56e
Joni Roine <joni.roine@outlook.com>
2023-12-21 14:39:37 +0200

    header image / adjusting

--- header.php
+++ header.php
@@ -5,15 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Joni Roine</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body class="me-5 ms-5 bg-light">
     <!-- NAVBAR -->
-    <nav class="navbar text-align-center navbar-expand-lg bg-transparent p-2 with-background">
 
+    <nav class="navbar text-align-center navbar-expand-lg p-5 rounded-bottom"
+        style="background-image: url('images/lunar.png');background-size: cover;">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -21,36 +22,24 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- Move the HOME button list item to the left -->
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                        <a class="nav-link active text-white border border-white rounded" aria-current="page"
+                            href="index.php">HOME</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-
                 </ul>
-                <form class="d-flex" role="search" onsubmit="search(event)">
+                <form class="d-flex">
+                    <!-- Place search input and button to the right -->
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-info" type="submit">Search</button>
                 </form>
             </div>
         </div>
     </nav>
+
     <div id="search-results"></div>
+
     <script src="javascript/search.js"></script>
 </body>
 
